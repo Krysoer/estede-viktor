6f04eedc852c5df0c0535ddbf6d3635e71b23440
Krysoer <michalekgolab2008@wp.pl>
2025-02-19 19:03:57 +0100

    All my homies hate templates

--- estedeViktor/viktor.hpp
+++ estedeViktor/viktor.hpp
@@ -1,196 +1,56 @@
 #pragma once
 #ifndef VIKTOR_HPP
 #define VIKTOR_HPP
+
 #include <stdexcept>
 #include <initializer_list>
+
 constexpr size_t STANDARD_VEC_SIZE = 10ll;
+
 namespace estede {
-	template <typename T>
-	class viktor {
-	private:
-		size_t capacity;//number of spaces allocated for type T
-		size_t size;//number of populated spaces
-		T* elements;//ptr to heap allocated c-style array
-	public:
-		viktor() {//default constructor, allocates 10 spaces for type T
-			this->size = 0ll;
-			this->capacity = STANDARD_VEC_SIZE;
-			this->elements = new T[capacity];
-		}
-		viktor(size_t capacity) {//allocates (capacity) spaces for type T
-			this->size = 0ll;
-			this->capacity = capacity;
-			this->elements = new T[capacity];
+    template <typename T>
+    class viktor {
+    private:
+        size_t capacity; // number of spaces allocated for type T
+        size_t size;     // number of populated spaces
+        T* elements;     // ptr to heap allocated c-style array
 
-		}
-		viktor(size_t length, const T& base) {//allocates (capacity) spaces for type T and populates them with (base)
-			this->size = length;
-			this->capacity = length*2ll;
-			this->elements = new T[capacity];
-			for (size_t i = 0ll; i < size; i++) {
-				elements[i] = base;
-			}
+    public:
+        // Constructors and Destructor
+        viktor(); // default constructor, allocates 10 spaces for type T
+        viktor(size_t capacity); // allocates (capacity) spaces for type T
+        viktor(size_t length, const T& base); // allocates (capacity) spaces for type T and populates them with (base)
+        viktor(const viktor& src); // copy constructor v(src)
+        viktor(std::initializer_list<T> list); // constructor from initializer list
+        ~viktor(); // cleans up the heap allocated array
 
-		}
-		viktor(const viktor& src) {//copy constructor v(src)
-			this->capacity = src.capacity;
-			this->elements = new T[capacity];
-			this->size = src.size;
-			for (size_t i = 0ll; i < size; i++) {
-				elements[i] = src.elements[i];
-			}
-		}
-		viktor(std::initializer_list<T> list) {
-			this->size = list.size();
-			this->capacity = this->size * 2ll;
-			this->elements = new T[capacity];
-			size_t iter = 0ll;
-			for (const T& val : list) {
-				elements[iter] = val;
-				iter++;
-			}
+        // Capacity and Size
+        bool empty() const; // returns true if no spaces are populated else returns false
+        size_t getsize() const; // returns the number of populated spaces
+        size_t getcapacity() const; // returns the number of spaces allocated for type T
 
-		}
-		~viktor() {//cleans up the heap allocated array
-			delete[] elements;
-		}
+        // Element Access
+        T& operator[](size_t index); // modify element at index
+        const T& operator[](size_t index) const; // get element at index
+        T& at(size_t index); // same as operator[] but with bounds check (throws std::out_of_range)
+        const T& at(size_t index) const; // same as operator[] but with bounds check (throws std::out_of_range)
 
-		bool empty() const {//returns true if no spaces are populated else returns false
-			return size == 0ll;
-		}
-		size_t getsize() const {//returns the number of populated spaces
-			return size;
-		}
-		size_t getcapacity() const {//returns the number of spaces allocated for type T
-			return capacity;
-		}
+        // Modifiers
+        void PushBack(const T& element); // Push element after the last
+        void PopBack(); // Remove last element (doesn't do any fancy deallocating, assumes the index may be still used in the future)
+        void Erase(size_t index); // erase element at index
+        void Insert(size_t index, const T& element); // inserts element at index
+        void Clear(); // clears array (doesn't do any fancy deallocating, assumes the allocated space may be still used in the future)
+        void ClearButFancy(); // deallocates the whole array
+        void Resize(size_t newSize); // resizes the array, throws std::out_of_range when less than current size
+        void ShrinkToFit(); // shrinks capacity to size
 
-		T& operator[](size_t index) {//modify element at index
-			return elements[index];
-		}
-		const T& operator[](size_t index) const {//get element at index
-			return elements[index];
-		}
-		viktor& operator=(const viktor& src) {//assigns the elements, size, and capacity (if dest is lower) from the right hand side viktor
-			if (this->capacity < src.capacity) {
-				this->capacity = src.capacity;
-			}
-			this->size = src.size;
-			delete[] elements;
-			this->elements = new T[capacity];
-			for (size_t i = 0ll; i < size; i++) {
-				this->elements[i] = src.elements[i];
-			}
-			return *this;
-		}
-		T& at(size_t index) {//same as operator[] but with bounds check (throws std::out_of_range)
-			if (index >= size) {
-				throw std::out_of_range("Attempted to access index out of bounds");
-			}
-			else [[likely]] {
-				return elements[index];
-				}
-		}
-		const T& at(size_t index) const {//same as operator[] but with bounds check (throws std::out_of_range)
-			if (index >= size) {
-				throw std::out_of_range("Attempted to access index out of bounds");
-			}
-			else [[likely]]{
-				return elements[index];
-			}
-		}
+        // Assignment Operator
+        viktor& operator=(const viktor& src); // assigns the elements, size, and capacity (if dest is lower) from the right hand side viktor
+    };
 
-		void PushBack(const T& element) {//Push element after the last
-			if (size == capacity) [[unlikely]]{
-				#ifdef _MSC_VER//MSVC resizes the array by 1.5 times while most other vector implementations (clang, GCC) resize by 2x
-				if (capacity < 2ll)
-					capacity = 2ll;
-				capacity *= 1.5;
-				T* temp = new T[capacity];
-				#else
-				if(capacity < 2ll)
-					capacity = 2ll;
-				capacity *= 2ll;
-				T* temp = new T[capacity];
-				#endif
-				for (size_t i = 0ll; i < size; i++) {
-					temp[i] = elements[i];
-				}
-				delete[] elements;
-				elements = temp;
-			}
-			elements[size] = element;
-			size++;
-		}
-		void PopBack() {//Remove last element (doesn't do any fancy deallocating, assumes the index may be still used in the future)
-			if (size > 0ll) [[likely]]
-				size--;
-			else
-				throw std::out_of_range("why dude");
-		}
-		void Erase(size_t index) {//erase element at index
-			for (size_t i = index; i < size-1ll; i++) {
-				elements[i] = elements[i + 1ll];
-			}
-			size--;
-		}
-		void Insert(size_t index, const T& element) {//inserts element at index
-			if (size == capacity) [[unlikely]]{
-				#ifdef _MSC_VER//MSVC resizes the array by 1.5 times while most other vector implementations (clang, GCC) resize by 2x
-				if (capacity < 2ll)
-					capacity = 2ll;
-				capacity *= 1.5;
-				T* temp = new T[capacity];
-				#else
-				if (capacity < 2ll)
-					capacity = 2ll;
-				capacity *= 2ll;
-				T* temp = new T[capacity];
-				#endif
-				for (size_t i = 0ll; i < size; i++) {
-					temp[i] = elements[i];
-				}
-				delete[] elements;
-				elements = temp;
-			}
-			for (size_t i = index; i < size; i++) {
-				elements[i + 1ll] = elements[i];
-			}
-			elements[index] = element;
-			size++;
-		}
-		void Clear() {//clears array (doesn't do any fancy deallocating, assumes the allocated space may be still used in the future)
-			size = 0;
-		}
-		void ClearButFancy() {//deallocates the whole array
-			size = 0ll;
-			capacity = 0ll;
-			T* temp = new T[capacity];
-			delete[] elements;
-			elements = temp;
-		}
-		void Resize(size_t newSize) {//resizes the array, throws std::out_of_range when less than current size
-			if (newSize >= size) [[likely]]{
-				this->capacity = newSize;
-				T* temp = new T[capacity];
-				for (size_t i = 0ll; i < size; i++) {
-					temp[i] = elements[i];
-				}
-				delete[] elements;
-				elements = temp;
-			}
-			else
-				throw std::out_of_range("Viktor resize failed");
-		}
-		void ShrinkToFit() {//shrinks capacity to size
-			capacity = size;
-			T* temp = new T[capacity];
-			for (size_t i = 0ll; i < size; i++) {
-				temp[i] = elements[i];
-			}
-			delete[] elements;
-			elements = temp;
-		}
-	};
+    // Include the template definitions
+    #include "viktor.inl"
 }
+
 #endif
---
+++ estedeViktor/viktor.inl
@@ -0,0 +1,209 @@
+// Constructors and Destructor
+template <typename T>
+estede::viktor<T>::viktor() : size(0ll), capacity(STANDARD_VEC_SIZE), elements(new T[capacity]) {}
+
+template <typename T>
+estede::viktor<T>::viktor(size_t capacity) : size(0ll), capacity(capacity), elements(new T[capacity]) {}
+
+template <typename T>
+estede::viktor<T>::viktor(size_t length, const T& base) : size(length), capacity(length * 2ll), elements(new T[capacity]) {
+    for (size_t i = 0ll; i < size; i++) {
+        elements[i] = base;
+    }
+}
+
+template <typename T>
+estede::viktor<T>::viktor(const viktor& src) : capacity(src.capacity), size(src.size), elements(new T[capacity]) {
+    for (size_t i = 0ll; i < size; i++) {
+        elements[i] = src.elements[i];
+    }
+}
+
+template <typename T>
+estede::viktor<T>::viktor(std::initializer_list<T> list) : size(list.size()), capacity(list.size() * 2), elements(new T[capacity]) {
+    size_t iter = 0ll;
+    for (const T& val : list) {
+        elements[iter] = val;
+        iter++;
+    }
+}
+
+template <typename T>
+estede::viktor<T>::~viktor() {
+    delete[] elements;
+}
+
+// Capacity and Size
+template <typename T>
+bool estede::viktor<T>::empty() const {
+    return size == 0ll;
+}
+
+template <typename T>
+size_t estede::viktor<T>::getsize() const {
+    return size;
+}
+
+template <typename T>
+size_t estede::viktor<T>::getcapacity() const {
+    return capacity;
+}
+
+// Element Access
+template <typename T>
+T& estede::viktor<T>::operator[](size_t index) {
+    return elements[index];
+}
+
+template <typename T>
+const T& estede::viktor<T>::operator[](size_t index) const {
+    return elements[index];
+}
+
+template <typename T>
+T& estede::viktor<T>::at(size_t index) {
+    if (index >= capacity) {
+        throw std::out_of_range("Attempted to access index out of bounds");
+    }
+    else [[likely]] {
+        return elements[index];
+        }
+}
+
+template <typename T>
+const T& estede::viktor<T>::at(size_t index) const {
+    if (index >= size) {
+        throw std::out_of_range("Attempted to access index out of bounds");
+    }
+    else [[likely]] {
+        return elements[index];
+        }
+}
+
+// Modifiers
+template <typename T>
+void estede::viktor<T>::PushBack(const T& element) {
+    if (size == capacity) [[unlikely]] {
+        // MSVC resizes the array by 1.5 times while most other vector implementations (clang, GCC) resize by 2x
+        #ifdef _MSC_VER
+        if (capacity < 2ll)
+            capacity = 2ll;
+        capacity *= 1.5;
+        T* temp = new T[capacity];
+        #else
+        if (capacity < 2ll)
+            capacity = 2ll;
+        capacity *= 2ll;
+        T* temp = new T[capacity];
+        #endif
+        for (size_t i = 0ll; i < size; i++) {
+            temp[i] = elements[i];
+        }
+        delete[] elements;
+        elements = temp;
+    }
+    elements[size] = element;
+    size++;
+}
+
+template <typename T>
+void estede::viktor<T>::PopBack() {
+    if (size > 0ll) [[likely]] {
+        size--;
+    }
+    else {
+        throw std::out_of_range("why dude");
+    }
+}
+
+template <typename T>
+void estede::viktor<T>::Erase(size_t index) {
+    for (size_t i = index; i < size - 1ll; i++) {
+        elements[i] = elements[i + 1ll];
+    }
+    size--;
+}
+
+template <typename T>
+void estede::viktor<T>::Insert(size_t index, const T& element) {
+    if (size == capacity) [[unlikely]] {
+        // MSVC resizes the array by 1.5 times while most other vector implementations (clang, GCC) resize by 2x
+        #ifdef _MSC_VER
+        if (capacity < 2ll)
+            capacity = 2ll;
+        capacity *= 1.5;
+        T* temp = new T[capacity];
+        #else
+        if (capacity < 2ll)
+            capacity = 2ll;
+        capacity *= 2ll;
+        T* temp = new T[capacity];
+        #endif
+        for (size_t i = 0ll; i < size; i++) {
+            temp[i] = elements[i];
+        }
+        delete[] elements;
+        elements = temp;
+    }
+    for (size_t i = index; i < size; i++) {
+        elements[i + 1ll] = elements[i];
+    }
+    elements[index] = element;
+    size++;
+}
+
+template <typename T>
+void estede::viktor<T>::Clear() {
+    size = 0;
+}
+
+template <typename T>
+void estede::viktor<T>::ClearButFancy() {
+    size = 0ll;
+    capacity = 0ll;
+    T* temp = new T[capacity];
+    delete[] elements;
+    elements = temp;
+}
+
+template <typename T>
+void estede::viktor<T>::Resize(size_t newSize) {
+    if (newSize >= size) [[likely]] {
+        capacity = newSize;
+        T* temp = new T[capacity];
+        for (size_t i = 0ll; i < size; i++) {
+            temp[i] = elements[i];
+        }
+        delete[] elements;
+        elements = temp;
+    }
+    else {
+        throw std::out_of_range("Viktor resize failed");
+    }
+}
+
+template <typename T>
+void estede::viktor<T>::ShrinkToFit() {
+    capacity = size;
+    T* temp = new T[capacity];
+    for (size_t i = 0ll; i < size; i++) {
+        temp[i] = elements[i];
+    }
+    delete[] elements;
+    elements = temp;
+}
+
+// Assignment Operator
+template <typename T>
+estede::viktor<T>& estede::viktor<T>::operator=(const viktor& src) {
+    if (this->capacity < src.capacity) {
+        this->capacity = src.capacity;
+    }
+    this->size = src.size;
+    delete[] elements;
+    this->elements = new T[capacity];
+    for (size_t i = 0ll; i < size; i++) {
+        this->elements[i] = src.elements[i];
+    }
+    return *this;
+}
